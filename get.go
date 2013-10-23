032a5144c1832dc295a9c5f501a3fc3036076ddc
Derek Chiang <derekchiang93@gmail.com>
2013-10-23 02:53:27 -0400

    Update get to use 0.2 client; add getAll

--- get.go
+++ get.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
-const GetUsage = `usage: etcdctl [etcd flags] get <key>`
+const GetUsage = `usage: etcdctl [etcd flags] get <key> [get flags]
+special flags: --sort set to true to return the result in sorted order`
+
+const GetAllUsage = `usage: etcdctl [etcd flags] getAll <key> [getAll flags]
+special flags: --sort set to true to return the result in sorted order`
+
+var (
+	getFlag = flag.NewFlagSet("get", flag.ExitOnError)
+	sorted  = getFlag.Bool("sort", false,
+		"Return the results in sorted order or not (default to false)")
+)
 
 func init() {
-	registerCommand("get", GetUsage, 2, 2, get)
+	registerCommand("get", GetUsage, 1, 2, get)
+	registerCommand("getAll", GetAllUsage, 1, 2, getAll)
 }
 
 func get(args []string) error {
-	key := args[1]
-	resps, err := client.Get(key)
+	key := args[0]
+	getFlag.Parse(args[1:])
+	resp, err := client.Get(key, *sorted)
 	if err != nil {
 		return err
 	}
-	for _, resp := range resps {
-		if resp.Value != "" {
-			fmt.Println(resp.Value)
-		}
+
+	if resp.Value != "" {
+		fmt.Println(resp.Value)
 	}
+
+	return nil
+}
+
+func getAll(args []string) error {
+	key := args[0]
+	getFlag.Parse(args[1:])
+	resp, err := client.GetAll(key, *sorted)
+	if err != nil {
+		return err
+	}
+
+	if resp.Value != "" {
+		fmt.Println(resp.Value)
+	}
+
 	return nil
 }
