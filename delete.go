d0d5501947ffbe8243e4b3e748193cb1795b39d8
Derek Chiang <derekchiang93@gmail.com>
2013-10-23 03:33:34 -0400

    Update delete to use 0.2 client; add deleteAll

--- delete.go
+++ delete.go
@@ -5,15 +5,29 @@ import (
 )
 
 const DeleteUsage = `usage: etcdctl [etcd flags] delete <key>`
+const DeleteAllUsage = `usage: etcdctl [etcd flags] deleteAll <key>`
 
 func init() {
-	registerCommand("delete", DeleteUsage, 2, 2, delete)
+	registerCommand("delete", DeleteUsage, 1, 1, delete)
+	registerCommand("deleteAll", DeleteAllUsage, 1, 1, deleteAll)
 }
 
 func delete(args []string) error {
-	key := args[1]
+	key := args[0]
 
 	resp, err := client.Delete(key)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.PrevValue)
+
+	return nil
+}
+
+func deleteAll(args []string) error {
+	key := args[0]
+
+	resp, err := client.DeleteAll(key)
 	if err != nil {
 		return err
 	}
