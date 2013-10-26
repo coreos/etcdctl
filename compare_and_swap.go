8a0e3eec2373481fe6e52f948c2269577039bc1f
Derek Chiang <derekchiang93@gmail.com>
2013-10-26 00:59:18 -0400

    Use 0.2 client for compare_and_swap

---
+++ compare_and_swap.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+const CompareAndSwapUsage = `usage: etcdctl [etcd flags] compareAndSwap <key> <value> [testAndSet flags]
+either prevValue or prevIndex needs to be given
+special flags: --ttl to set a key with ttl
+			   --pvalue to set the previous value
+			   --pindex to set the previous index`
+
+var (
+	compareAndSwapFlag   = flag.NewFlagSet("testAndSet", flag.ExitOnError)
+	compareAndSwapTtl    = compareAndSwapFlag.Uint64("ttl", 0, "ttl of the key")
+	compareAndSwapPvalue = compareAndSwapFlag.String("pvalue", "", "previous value")
+	compareAndSwapPindex = compareAndSwapFlag.Uint64("pindex", 0, "previous index")
+)
+
+func init() {
+	// The minimum number of arguments is 3 because
+	// there needs to be either pvalue or pindex
+	registerCommand("compareAndSwap", CompareAndSwapUsage, 3, 6, compareAndSwap)
+}
+
+func compareAndSwap(args []string) error {
+	key := args[0]
+	value := args[1]
+	compareAndSwapFlag.Parse(args[2:])
+	resp, err := client.CompareAndSwap(key, value,
+		*compareAndSwapTtl, *compareAndSwapPvalue, *compareAndSwapPindex)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(resp.Value)
+	return nil
+}
--- watch.go
+++ watch.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/coreos/etcd/store"
+	"github.com/coreos/go-etcd/etcd"
 	"os"
 	"os/signal"
 )
 
 const WatchUsage = `usage: etcdctl [etcd flags] watch <key> [watch flags]
+special flags: -f forever watch a key until CTRL+C
+               -i watch from the given index`
+
+const WatchAllUsage = `usage: etcdctl [etcd flags] watchAll <key> [watchAll flags]
 special flags: -f forever watch a key until CTRL+C
                -i watch from the given index`
 
@@ -19,12 +23,13 @@ var (
 )
 
 func init() {
-	registerCommand("watch", WatchUsage, 2, 6, watch)
+	registerCommand("watch", WatchUsage, 1, 3, watch)
+	registerCommand("watchAll", WatchAllUsage, 1, 3, watchAll)
 }
 
 func watch(args []string) error {
-	key := args[1]
-	watchFlag.Parse(args[2:])
+	key := args[0]
+	watchFlag.Parse(args[1:])
 
 	if *forever {
 
@@ -39,7 +44,7 @@ func watch(args []string) error {
 			os.Exit(0)
 		}()
 
-		receiver := make(chan *store.Response)
+		receiver := make(chan *etcd.Response)
 		go client.Watch(key, uint64(*index), receiver, stop)
 
 		for {
@@ -49,6 +54,42 @@ func watch(args []string) error {
 
 	} else {
 		resp, err := client.Watch(key, uint64(*index), nil, nil)
+		if err != nil {
+			return err
+		}
+		fmt.Println(resp.Action, " ", resp.Key, " ", resp.Value)
+	}
+
+	return nil
+}
+
+func watchAll(args []string) error {
+	key := args[0]
+	watchFlag.Parse(args[1:])
+
+	if *forever {
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+
+		stop := make(chan bool)
+
+		go func() {
+			<-c
+			stop <- true
+			os.Exit(0)
+		}()
+
+		receiver := make(chan *etcd.Response)
+		go client.WatchAll(key, uint64(*index), receiver, stop)
+
+		for {
+			resp := <-receiver
+			fmt.Println(resp.Action, " ", resp.Key, " ", resp.Value)
+		}
+
+	} else {
+		resp, err := client.WatchAll(key, uint64(*index), nil, nil)
 		if err != nil {
 			return err
 		}
