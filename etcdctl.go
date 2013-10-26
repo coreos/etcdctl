d88838fa37f50ec3d7d612ab3912df18e397ac9d
Derek Chiang <derekchiang93@gmail.com>
2013-10-26 01:11:45 -0400

    Supporting JSON output

--- etcdctl.go
+++ etcdctl.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
@@ -8,16 +9,28 @@ import (
 )
 
 var (
-	client *etcd.Client
-
+	client       *etcd.Client
 	printVersion bool
+	outputJSON   bool
+	cluster      = ClusterValue{"http://localhost:4001"}
 )
+
+func output(resp *etcd.Response) {
+	if outputJSON {
+		b, err := json.Marshal(resp)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", b)
+	} else {
+		fmt.Println(resp.Value)
+	}
+}
 
 func main() {
 	flag.BoolVar(&printVersion, "version", false, "print the version and exit")
-
-	cluster := ClusterValue{"http://localhost:4001"}
 	flag.Var(&cluster, "C", "a comma seperated list of machine addresses in the cluster e.g. 127.0.0.1:4001,127.0.0.1:4002")
+	flag.BoolVar(&outputJSON, "json", false, "Output server response in JSON format")
 	flag.Parse()
 
 	if printVersion {
@@ -42,7 +55,9 @@ func main() {
 		os.Exit(MalformedEtcdctlArguments)
 	}
 
-	if len(args) > command.maxArgs || len(args) < command.minArgs {
+	commandArgs := args[1:]
+
+	if len(commandArgs) > command.maxArgs || len(commandArgs) < command.minArgs {
 		fmt.Println("wrong arguments provided")
 		fmt.Println(command.usage)
 		os.Exit(MalformedEtcdctlArguments)
@@ -53,9 +68,10 @@ func main() {
 		os.Exit(FailedToConnectToHost)
 	}
 
-	err := command.f(args)
+	err := command.f(commandArgs)
 
 	if err != nil {
+		fmt.Print("Error: ")
 		fmt.Println(err)
 		os.Exit(ErrorFromEtcd)
 	}
