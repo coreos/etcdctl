2d06fc91077e48bcda0ea8800f935138d0b8ce6e
Brandon Philips <brandon@ifup.co>
2013-11-29 17:24:21 -0800

    feat(command): change extended to use RFC 2822 format
    In the effort not to invent new things lets use a simpler RFC 2822
    format for the extended output of etcdctl.

--- examples/do-on-change.sh
+++ examples/do-on-change.sh
@@ -6,14 +6,14 @@
 # The key to watch
 KEY=config
 
-index_re='s/^# modified-index: \(.*\)/\1/p'
+index_re='s/^Modified-Index: \(.*\)/\1/p'
 
 index=$(./etcdctl -o extended get ${KEY} | sed -n -e "${index_re}")
 
 while true; do
 	out=$(./etcdctl -o extended watch ${KEY} --after-index ${index})
-	config=$(echo "${out}" | grep -v "^#")
 	index=$(echo "${out}" | sed -n -e "${index_re}")
+	config=$(echo "${out}" | sed -e '1,/^$/d')
 
 	# Print out the example command line to execute
 	echo "echo \"${config}\" | config-update"
