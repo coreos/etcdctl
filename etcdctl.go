86a8dc0378c37ac5103f87bfa8255db680c4071b
Andrew Forgue <andrew@forgue.io>
2014-07-04 15:25:51 -0700

    Peer configuration via ETCDCTL_PEERS env variable

--- etcdctl.go
+++ etcdctl.go
@@ -16,7 +16,7 @@ func main() {
 		cli.BoolFlag{"debug", "output cURL commands which can be used to reproduce the request"},
 		cli.BoolFlag{"no-sync", "don't synchronize cluster information before sending request"},
 		cli.StringFlag{"output, o", "simple", "output response in the given format (`simple` or `json`)"},
-		cli.StringSliceFlag{"peers, C", &cli.StringSlice{}, "a comma-delimited list of machine addresses in the cluster (default: {\"127.0.0.1:4001\"})"},
+		cli.StringSliceFlag{"peers, C", &cli.StringSlice{}, "a list of peers in the cluster, specify multiple times for multiple peers. (default: 127.0.0.1:4001 or ETCDCTL_PEERS environment variable)"},
 	}
 	app.Commands = []cli.Command{
 		command.NewMakeCommand(),
