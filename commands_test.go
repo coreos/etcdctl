7f2ee8504eebe4449cfbce125f8cbc76c97c3ce8
Shantanu <joshishantanu4@gmail.com>
2014-07-18 13:39:19 -0700

    regex testing under way

--- commands_test.go
+++ commands_test.go
@@ -16,11 +16,56 @@ type testFormat struct {
 	commandLine *exec.Cmd
 }
 
+//Testing story so far:
+/*
+1. set a key
+2. get a key.
+3. ls and ls --recursive.
+4. test update
+5. test mkdir.
+*/
+
+var keyPattern string = "^[a-z]([A-Za-z0-9]*)"
+
+// key rules assumed only for testing: starts wiht lower case, after that can contain letters and numbers.
+var lsRecursivePattern string = "^(/([a-z][A-Za-z0-9]*)*\n?)+"
+
+// recursiveDirPattern  is of the form (/keypattern)+
+var lsPattern string = "^(/([a-z]([A-Za-z0-9]*))\n)+(/[a-z]([A-Za-z0-9]*))"
+
 var testCases []testFormat = []testFormat{
 	testFormat{
-		stdoutRegX: regexp.MustCompile("([a-z][a-z0-9]*)*"),
-		// stderrRegX:  regexp.MustCompile(""),
+		stdoutRegX:  regexp.MustCompile(keyPattern),
 		commandLine: exec.Command(CMD, "set", "/foo", "bar"),
+	},
+
+	testFormat{
+		stdoutRegX:  regexp.MustCompile(keyPattern),
+		commandLine: exec.Command(CMD, "set", "/coreOS/keys/dog", "coreo"),
+	},
+
+	testFormat{
+
+		stdoutRegX:  regexp.MustCompile(keyPattern),
+		commandLine: exec.Command(CMD, "get", "/foo"),
+	},
+
+	testFormat{
+
+		stdoutRegX:  regexp.MustCompile(keyPattern),
+		commandLine: exec.Command(CMD, "get", "/coreOS/keys/dog"),
+	},
+
+	testFormat{
+
+		stdoutRegX:  regexp.MustCompile(lsRecursivePattern),
+		commandLine: exec.Command(CMD, "ls", "--recursive"),
+	},
+
+	testFormat{
+
+		stdoutRegX:  regexp.MustCompile(lsPattern),
+		commandLine: exec.Command(CMD, "ls"),
 	},
 }
 
@@ -45,7 +90,7 @@ func TestAll(t *testing.T) {
 		n, err := stderr.Read(buf)
 		if n > 0 {
 			t.Fail()
-			t.Log(CToGoString(buf[:n]))
+			t.Log(ByteSliceToString(buf[:n]))
 		}
 
 		if err != nil && n > 0 {
@@ -54,7 +99,7 @@ func TestAll(t *testing.T) {
 
 		bufO := make([]byte, 100, 100)
 		stdout.Read(bufO)
-
+		// fmt.Println(ByteSliceToString(bufO[:m]))
 		if tst.stdoutRegX.Match(bufO) == false {
 			t.Fail()
 			t.Logf("Stdout pattern does not match for test number %d", index)
@@ -67,66 +112,9 @@ func TestAll(t *testing.T) {
 	}
 }
 
-// functional tests: requires you to have etcd running on your machine locally.
-// also requires a etcdctl executable which is located in your $GOBIN ( which should be a part of your #PATH)
-// further assumption is that etcd starts of without carrying over any information from previous runs.
-
-//Will test the simple: etcdctl ls and etcdctl ls --recursive versions of the flag.
-// func TestLscommandEmpty(t *testing.T) {
-// 	output, err := exec.Command(CMD, "ls").Output()
-// 	if len(output) != 0 {
-// 		fmt.Println(output)
-// 		t.Error("Error! reported number of keys > 0 but no keys have been added so far")
-// 	}
-
-// 	if err != nil {
-// 		t.Error("etcdctl ls encountered an error ! ")
-// 	}
-// }
-
-// func TestGetcommand(t *testing.T) {
-
-// 	cmd := exec.Command("etcdctl", "get", "/coreOS/keys/barz")
-// 	stdout, err := cmd.StdoutPipe()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	stderr, err := cmd.StderrPipe()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	if err := cmd.Start(); err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	buf := make([]byte, 100, 100)
-// 	n, err := stderr.Read(buf)
-// 	if n > 0 {
-// 		t.Error(CToGoString(buf[:n]))
-// 	}
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	n, _ = stdout.Read(buf)
-
-// 	if n > 0 {
-// 		fmt.Println("success: ")
-// 		fmt.Println(CToGoString(buf[:n]))
-
-// 	}
-// 	if err := cmd.Wait(); err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// }
-
 // from stackoverflow:
 // http://stackoverflow.com/questions/14230145/what-is-the-best-way-to-convert-byte-array-to-string
-func CToGoString(c []byte) string {
+func ByteSliceToString(c []byte) string {
 	n := -1
 	for i, b := range c {
 		if b == 0 {
@@ -136,5 +124,3 @@ func CToGoString(c []byte) string {
 	}
 	return string(c[:n+1])
 }
-
-//get prints its errors to Stderr and output to Stdout.
